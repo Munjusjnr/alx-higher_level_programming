eb1122b750b1383866be6e2b4147dc14d04a2e4b
Munjusjnr <munjusjnr54@outlook.com>
2023-05-09 08:25:45 -0700

    last digit script to realize negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 lastdigit = abs(number) % 10
+if number < 0:
+    lastdigit *= -1
 if lastdigit > 5:
     print(f"Last digit of {number:d} is {lastdigit:d} and is greater than 5")
 elif lastdigit < 6:
