d57ff6627e1888f41906bafc58949721e947a65d
Munjusjnr <munjusjnr54@outlook.com>
2023-05-09 11:55:14 -0700

    changes made to range

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
-    print("{:02d}".format(numbers), end="")
-    if numbers != 99:
-        print(", ", end="")
-    else:
-        print()
+    if numbers >= 0 and numbers < 99:
+        print("{:02d}".format(numbers), end=", ")
+    if numbers == 99:
+        print("99")
