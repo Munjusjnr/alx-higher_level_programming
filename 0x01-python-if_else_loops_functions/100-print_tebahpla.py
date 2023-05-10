f7b8496db9fd0f9e12d50fc2b660a2a719844fda
Munjusjnr <munjusjnr54@outlook.com>
2023-05-10 09:39:09 -0700

    changes made to python script

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for letters in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(letters) if letters % 2 == 0 else letters - 32, end="")
+    print("{:c}".format(letters)if letters % 2 == 0
+          else chr(letters - 32), end="")
