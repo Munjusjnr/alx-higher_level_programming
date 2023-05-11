d0a4cbbe7b6a42662eb991d3ea454be921ad6901
Munjusjnr <munjusjnr54@outlook.com>
2023-05-11 15:38:36 -0700

    operator check conditions improved

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2] not in ['+' or '-' or '*' or '/']:
+    if sys.argv[2] not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     else:
