c9fa70e8205078fd534bcefe2856d271a1d8bcb6
Munjusjnr <munjusjnr54@outlook.com>
2023-05-21 16:00:14 -0700

    requirement met for dict

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value not in a_dictionary:
-        return a_dictionary
-    else:
-        del a_dictionary[value]
-        return a_dictionary
+    deleted_keys = [key for key, val in a_dictionary.items() if val == value]
+    for key in deleted_keys:
+        del a_dictionary[key]
+    return a_dictionary
