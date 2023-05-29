4c1a4a18aec8985ad47defebf2ae76b2d5d70e9b
Munjusjnr <munjusjnr54@outlook.com>
2023-05-29 07:11:36 -0700

    printing intergers in a list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# This function prints the first x(integers) of a list
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+    except IndexError:
+        pass
+    print()
+    return count
