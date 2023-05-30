5e09d819dde59474ba1d34c86ca8da79c4df8fd2
Munjusjnr <munjusjnr54@outlook.com>
2023-05-30 11:15:56 -0700

    accessing and updating private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+""" Defining a class square """
+
+
+class Square:
+    """
+    A class representing a square
+
+    Attributes:
+        size: A private instance with an optional type
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        """
+        condition to check instantiation with optional meets int criteria
+
+        Another condition to check whether value is a real number
+
+        """
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ A public instance method that returns the area of the square """
+        return self.__size * self.__size
