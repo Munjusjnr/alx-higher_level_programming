b54eb887c21d0e1b865225d7201cd452e3ee3bf1
Munjusjnr <munjusjnr54@outlook.com>
2023-05-30 08:21:43 -0700

    a square with private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Defining a class square """
+
+
+class Square:
+    """
+    A class representing a square
+
+    Attributes:
+        size: A private instance attribute with no type.
+    """
+    def __init__(self, size):
+        self.__size = size
