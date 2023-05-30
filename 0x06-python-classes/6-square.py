25b2ed1bcb1bd3a356be02fc7bccf47657f91713
Munjusjnr <munjusjnr54@outlook.com>
2023-05-30 12:21:20 -0700

    coordinates of a square with private instance position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+""" Defining a class square """
+
+
+class Square:
+    """
+    A class representing a square
+
+    Attributes:
+        size: A private instance with an optional type
+        position: A private instance with tuple-like values
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+        """
+        condition to check instantiation with optional meets int criteria
+
+        Another condition to check whether value is a real number
+
+        """
+    @property
+    def size(self):
+        """ size getter
+            Return:
+                size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ size setter
+            Return:
+                value of square size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ position getter
+            Return:
+                position of the square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ position setter and Returns position value of a square """
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """ A public instance method that returns the area of the square """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ A public instance method that prints character # when called """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                if self.__position[0] > 0:
+                    print(" " * self.__position[0], end="")
+                print("#" * self.__size)
