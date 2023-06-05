121bff1cf9eeeda3cfbefc1945d504b68ee70f0f
Munjusjnr <munjusjnr54@outlook.com>
2023-06-05 05:57:41 -0700

    another condtion added to check for value being 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -76,4 +76,7 @@ class Rectangle:
 
     def perimeter(self):
         """ A public instance that returns the perimeter of the rectangle """
-        return self.__width + self.__height + self.__width + self.__height
+        per = (self.__width + self.__height) * 2
+        if self.__width == 0 or self.__height == 0:
+            per == 0
+        return per
