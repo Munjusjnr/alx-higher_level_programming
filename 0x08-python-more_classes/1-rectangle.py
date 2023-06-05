36c6d3f277a5b17cfb076f66c24b8e692ce5ea1f
Munjusjnr <munjusjnr54@outlook.com>
2023-06-05 05:36:54 -0700

    condition to check for width and height being int

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,6 +14,21 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
+        """
+        condition to check whether height or width meets int criteria
+
+        Also checking the value to be a real number
+
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
 
     @property
     def width(self):
