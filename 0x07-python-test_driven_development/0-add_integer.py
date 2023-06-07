a564988b36bc1c0dbef32a44d850971ddd3df210
Munjusjnr <munjusjnr54@outlook.com>
2023-06-07 05:25:21 -0700

    being able to handle large float numbers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,10 +13,15 @@ def add_integer(a, b=98):
     Return: the sum of the two parameters
 
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
-    return a + b
+    try:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+        a = int(a) if isinstance(a, float) else a
+
+        b = int(b) if isinstance(b, float) else b
+
+        return a + b
+    except TypeError as error:
+        raise TypeError(error)
