8510a744b36de2ca269e182b56562f0b43c891ad
Munjusjnr <munjusjnr54@outlook.com>
2023-06-12 05:53:56 -0700

    returning a boolean value if an object is an instance of a specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" A function that uses boolean mode to determine a class """
+
+
+def is_same_class(obj, a_class):
+    """ Checking if object is an instance of a specified class
+    Boolean values:
+        True: if it is
+        False: if otherwise
+    """
+    return True if isinstance(obj, a_class) else False
