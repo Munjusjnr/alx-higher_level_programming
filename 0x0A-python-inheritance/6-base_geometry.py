2dfe05721edc150ed69eff0dba930223f973f68c
Munjusjnr <munjusjnr54@outlook.com>
2023-06-12 07:28:35 -0700

    improving the previous class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Defining a class BaseGeometry """
+
+
+class BaseGeometry:
+    """ A class of BaseGeometry """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """ A public instance method """
+        raise Exception("area() is not implemented")
