ad91237983d1907153c7347d9e0d396b171f7c7f
Munjusjnr <munjusjnr54@outlook.com>
2023-06-13 11:18:18 -0700

    using import differently

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,22 +2,19 @@
 
 """ Modules to be imported """
 import sys
-from 5-save_to_json_file.py import save_to_json_file
-from 6-load_from_json_file.py import load_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def main():
+def argument_to_list(args):
     """ getting the command line arguments excluding script name """
     args = sys.argv[1:]
-
-    """ list the hold all arguments """
-    my_list = []
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
 
     my_list.extend(args)
 
     """ saving to required filename """
     save_to_json_file(my_list, "add_item.json")
-
-
-if __name__ == "__main__":
-    main()
