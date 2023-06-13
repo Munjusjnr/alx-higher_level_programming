90d67772b790df19d268c79ba96863b2245baf10
Munjusjnr <munjusjnr54@outlook.com>
2023-06-13 11:29:31 -0700

    update of code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def argument_to_list(args):
-    """ getting the command line arguments excluding script name """
-    args = sys.argv[1:]
+    """ Add command-line arguments to list and save to JSON file
+
+        Arguments:
+            args: command-line arguments
+    """
     try:
         my_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
@@ -18,3 +21,10 @@ def argument_to_list(args):
 
     """ saving to required filename """
     save_to_json_file(my_list, "add_item.json")
+
+
+""" command-line arguments without script name """
+args = sys.argv[1:]
+
+""" adding arguments to list and saving to JSON File """
+argument_to_list(args)
