ab7734bf2a964dfb5d0557de45115e2b4ca6234d
Munjusjnr <munjusjnr54@outlook.com>
2023-06-13 08:27:46 -0700

    docstring added

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,4 +5,11 @@ import json
 
 
 def to_json_string(my_obj):
+    """ The JSON representation of an object
+
+        Args:
+            my_obj: The object to be serialized into JSON
+        Return:
+            the JSON representation of an object
+    """
     return json.dumps(my_obj)
