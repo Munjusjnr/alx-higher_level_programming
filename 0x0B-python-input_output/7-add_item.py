fbd91d353cb20da6b21e4351f980adbc1b0a316e
Munjusjnr <munjusjnr54@outlook.com>
2023-06-13 10:58:46 -0700

    using import rather

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,32 +2,8 @@
 
 """ Modules to be imported """
 import sys
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """ Serialized an object to its JSON representation and writes it
-    to a text file
-
-    Args:
-        my_obj: Object to be serialized
-        filename: Path the file where the object would be written
-    """
-    with open(filename, mode='w', encoding="UTF8") as f:
-        f.write(json.dumps(my_obj))
-
-
-def load_from_json_file(filename):
-    """ Deserializing a JSON file and creating an object
-
-    Args:
-        filename: path to the file where the objects would be created from
-
-    Return: objects created
-    """
-    with open(filename, encoding="UTF8") as f:
-        my_data = json.load(f)
-    return my_data
+from 5-save_to_json_file.py import save_to_json_file
+from 6-load_from_json_file.py import load_to_json_file
 
 
 def main():
