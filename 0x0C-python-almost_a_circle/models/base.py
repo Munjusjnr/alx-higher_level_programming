093374cdfd883b152c369aba9bd5e8e7331e3e61
Munjusjnr <munjusjnr54@outlook.com>
2023-06-18 07:48:26 -0700

    updates on base.py with json serialization and deserialization

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """ Defining the first class, Base, for other classes """
+import json
 
 
 class Base:
@@ -12,8 +13,58 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Initializer """
         if id is not None:
             self.id = id
         else:
             base.__nb_object += 1
             self.id = self.__nb_object
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the json string representation of list_dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ A class method that writes the JSON string rep. to a file """
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        with open(filename, mode='w', encoding="UTF8") as f:
+            json_str = cls.to_json_string([obj.to_dictionary() for obj in
+                                          list_objs])
+            f.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ A static method that returns the list of JSON string rep. """
+        if json is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Returns an instance with all attributes already set """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ Returning a list of instances """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, mod='w', encoding="UTF8") as f:
+                t = cls.from_json_string(f.read())
+                instance = [cls.create(**dictionary) for dictionary in t]
+                return instance
+        except FileNotFoundError:
+            return []
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -112,7 +112,7 @@ class Rectangle(Base):
         return ("[Rectangle] {(:d)} {:d}/{:d} - {:d}/{:d}".format(self.id,
                 self.__x, self.__y, self.__width, self.__height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ method that assigns an argument to each attribute """
         if args:
             if len(args) > 0:
