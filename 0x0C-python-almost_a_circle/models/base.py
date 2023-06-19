d6fe6fc5e333b6a928fc9407a476b67492dd17df
Munjusjnr <munjusjnr54@outlook.com>
2023-06-19 08:15:42 -0700

    grammatical error fixed in base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,7 +42,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ A static method that returns the list of JSON string rep. """
-        if json is None or len(json_string) == 0:
+        if json_string is None or len(json_string) == 0:
             return []
         else:
             return json.loads(json_string)
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,4 +8,29 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     def setUp(self):
+        """ Testing with sample rectangle """
+        self.rectangle = Rectangle(1, 5, 8, 9, 4)
 
+    def test_id(self):
+        """ Testing with id of the rectangle """
+        self.assertEqual(self.rectangle.id, 1)
+
+    def test_width(self):
+        """ Testing the width of the rectangle """
+        self.assertEqual(self.rectangle.width, 5)
+
+    def test_height(self):
+        """ Testing the height of the rectangle """
+        self.assertEqual(self.rectangle.height, 8)
+
+    def test_x(self):
+        """ Testing for x functionality """
+        self.assertEqual(self.rectangle.x, 9)
+
+    def test_y(self):
+        """ Testing for y functionality """
+        self.assertEqual(self.rectangle.y, 4)
+
+    def test_area(self):
+        """ Testing for the area of the rectangle """
+        self.assertEqual(self.rectange.area, 40)
