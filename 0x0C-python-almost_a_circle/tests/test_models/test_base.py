70077762d163cb3c04c2b6335e329773b3177738
Munjusjnr <munjusjnr54@outlook.com>
2023-06-19 07:44:04 -0700

    test cases for class bases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,4 +6,33 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
+    def test_with_id(self):
+        """ Testing initializer with given id """
+        b = Base(id=12)
+        self.assertEqual(b.id, 12)
+
+    def test_without_id(self):
+        """ Testing initializer without id """
+        first = Base()
+        second = Base()
+        self.assertEqual(first.id, second.id)
+
+    def test_negative_id(self):
+        """ Testing initializer with negative id """
+        with self.assertRaises(ValueError):
+            b = Base(id=-8)
+
+    def test_with_noninteger_id(self):
+        """ Testing initializer with non integer id """
+        with self.assertRaises(TypeError):
+            b = Base(id="wonder")
+
+    def test_nb_objects_increment(self):
+        """ Testing nb_objects incrementing correctly """
+        first = Base()
+        second = Base()
+        self.assertEqual(second.id, first.id + 1)
+
+
+
 
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Containing tests for the class rectangle which inherits another class """
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+
