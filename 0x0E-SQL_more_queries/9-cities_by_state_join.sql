73da04152f9d51aa9302540852cd069b507381d5
Munjusjnr <munjusjnr54@outlook.com>
2023-07-19 09:43:32 -0700

    using the join command to list linked elements

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa
+-- using the join command
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON states.id = citites.state_id
+ORDER BY cities.id ASC;
