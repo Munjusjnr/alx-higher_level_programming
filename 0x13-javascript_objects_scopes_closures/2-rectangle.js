799fcd81163c646f90674ed92a9c9d922a8abfc4
Munjusjnr <munjusjnr54@outlook.com>
2023-08-15 05:43:00 -0700

    update with another file

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,11 +4,14 @@
  * The constructor must take 2 arguments w and h
  * Initialize the instance attribute width with the value of w
  * Initialize the instance attribute height with the value of h
+ * create an empty object if with or height is not a real number
  */
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 };
