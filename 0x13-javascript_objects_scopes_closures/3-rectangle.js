b6ac490f14073635ccb4dd2f2301470f22965910
Munjusjnr <munjusjnr54@outlook.com>
2023-08-15 05:58:15 -0700

    printing out the rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/*
+ * A class defining rectangle
+ * The constructor must take 2 arguments w and h
+ * Initialize the instance attribute width with the value of w
+ * Initialize the instance attribute height with the value of h
+ * create an empty object if with or height is not a real number
+ */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  // An instance method called print
+
+  print () {
+    if (this.width > 0 && this.height > 0) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+};
