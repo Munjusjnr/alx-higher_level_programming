599ccab0fa351bbc8eb7ab724686e3f2f7b15e91
Munjusjnr <munjusjnr54@outlook.com>
2023-08-15 04:49:47 -0700

    defining a class rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * A class defining rectangle
+ * The constructor must take 2 arguments w and h
+ * Initialize the instance attribute width with the value of w
+ * Initialize the instance attribute height with the value of h
+ */
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
