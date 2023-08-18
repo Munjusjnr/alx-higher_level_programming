6c646a2cb9a649453c48e78800ae83e9846ef326
Munjusjnr <munjusjnr54@outlook.com>
2023-08-18 15:03:30 -0700

    getting all states from a database using python scripts

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#  A script that lists all states from the database hbtn_0e_0_usa
+""" script should take 3 arguments: mysql username, mysql password and
+    database name (no argument validation needed
+    Your code should not be executed when imported
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states`")
+    results = cur.fetchall()
+    for row in results:
+        print(row)
