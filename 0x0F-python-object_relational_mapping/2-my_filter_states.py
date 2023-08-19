d130456d7fe0f3c3c6926b5d2fd179138115c8fe
Munjusjnr <munjusjnr54@outlook.com>
2023-08-19 07:33:50 -0700

    filtering data by user inputs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# A script that lists all states from the database hbtn_0e_0_usa
+#  A script that lists all states from the database hbtn_0e_0_usa
 """ script should take 3 arguments: mysql username, mysql password and
     database name (no argument validation needed
     Your code should not be executed when imported
@@ -12,12 +12,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id`")
-    names = set()
-    results = cur.fetchall()
+    state_name = sys.argv[4]
+    query = "SELECT * FROM `states` WHERE BINARY `name` = %s"
+    cur.execute(query, (state_name,))
 
+    results = cur.fetchall()
     for row in results:
-        state_id, state_name = row
-        if state_name not in names:
-            names.add(state_name)
-            print(row)
+        print(row)
