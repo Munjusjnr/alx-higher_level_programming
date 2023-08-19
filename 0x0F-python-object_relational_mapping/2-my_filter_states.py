5a65b460f5cde366ee7e6008a3609a0ec789033a
Munjusjnr <munjusjnr54@outlook.com>
2023-08-19 07:46:56 -0700

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 #  A script that lists all states from the database hbtn_0e_0_usa
-""" script should take 3 arguments: mysql username, mysql password and
-    database name (no argument validation needed
+""" script should take 4 arguments: mysql username, mysql password
+    database name and state name searched (no argument validation needed
+    You must use the module MySQLdb
+    Your script should connect to a MySQL server running on localhost at port
     Your code should not be executed when imported
 """
 import sys
@@ -12,10 +14,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    state_name = sys.argv[4]
-    query = "SELECT * FROM `states` WHERE BINARY `name` = %s"
-    cur.execute(query, (state_name,))
-
-    results = cur.fetchall()
-    for row in results:
-        print(row)
+    cur.execute("SELECT * FROM states WHERE BINARY `name` = '{}'"
+                .format(sys.argv[4]))
+    [print(row) for row in cur.fetchall()]
