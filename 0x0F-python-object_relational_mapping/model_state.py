4e4d45da7959b3dcbe676f2aef43ea0b7a935424
Munjusjnr <munjusjnr54@outlook.com>
2023-08-20 07:43:16 -0700

    all states via sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,13 +8,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Represents the class definition of a state
-    id: class attribute id that represents a column of an auto-generated, unique integer,
-       can’t be null and is a primary key
-    name: class attribute name that represents a column of a string with 
+    id: class attribute id that represents a column of an auto-generated,
+       unique integer, can’t be null and is a primary key
+    name: class attribute name that represents a column of a string with
         max 128 chars and can’t be null
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
