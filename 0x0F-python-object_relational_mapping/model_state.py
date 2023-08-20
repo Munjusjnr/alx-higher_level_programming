1fe111eef48ca149ef737296079947206a480541
Munjusjnr <munjusjnr54@outlook.com>
2023-08-20 06:04:21 -0700

    using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defining a class state that inherits from base
+Will use the SQLAlchemy module
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Represents the class definition of a state
+    id: class attribute id that represents a column of an auto-generated, unique integer,
+       can’t be null and is a primary key
+    name: class attribute name that represents a column of a string with 
+        max 128 chars and can’t be null
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(128), nullable=False)
