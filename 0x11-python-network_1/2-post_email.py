0b8e2a8e27dacff9f48b4be72e772a79a2d0a5fe
Munjusjnr <munjusjnr54@outlook.com>
2023-09-01 12:46:43 -0700

    update of POST file

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Modules to be used for the POST request"""
-import urllib.request
-import urllib.parse
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 import sys
 
 
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     response (decoded in utf-8)
     """
     url = sys.argv[1]
-    email = sys.argv[2]
+    email = {'email': sys.argv[2]}
 
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
+    data = urlencode(email).encode('utf-8')
+    request = Request(url, data)
+    with urlopen(request) as response:
         body = response.read()
         print(body.decode("utf-8"))
