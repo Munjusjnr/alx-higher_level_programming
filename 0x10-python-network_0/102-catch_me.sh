07788f48d8a05024ee5cbb505d0d1d3b540aca53
Munjusjnr <munjusjnr54@outlook.com>
2023-09-04 13:38:17 -0700

    code updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message in the body of a response
-curl -s -L -X PUT -H -d "user_id=98" 0.0.0.0:5000/catch_me 
+curl -s -L -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
